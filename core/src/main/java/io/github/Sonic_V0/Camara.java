89fa483511955ad5f5df571b9be1b0b990839bb8
aarom <aaromarmando3@gmail.com>
2025-07-04 16:41:35 -0400

    4/7/25

--- core/src/main/java/io/github/Sonic_V0/Camara.java
+++ core/src/main/java/io/github/Sonic_V0/Camara.java
@@ -1,6 +1,8 @@
 package io.github.Sonic_V0;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class Camara {
     private final OrthographicCamera camara;
@@ -13,4 +15,14 @@ public class Camara {
     public OrthographicCamera getCamara() {
         return camara;
     }
+
+    public Vector3 getProject (float cx, float cy, float cz) {
+        return  camara.project(new Vector3(cx,cy,cz));
+    }
+
+    public Rectangle getVistaRectangular() {
+        return new Rectangle(camara.position.x - camara.viewportWidth / 2f,
+            camara.position.y - camara.viewportHeight / 2f, camara.viewportWidth, camara.viewportHeight);
+    }
+
 }
