37f42e2b01f5c5f03fa11382df46db3d1d5be976
aarom <aaromarmando3@gmail.com>
2025-07-14 07:50:17 -0400

    14/7/25

--- core/src/main/java/io/github/Sonic_V0/Camara.java
+++ core/src/main/java/io/github/Sonic_V0/Camara.java
@@ -3,18 +3,12 @@ package io.github.Sonic_V0;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-
-
 
 public class Camara {
-   // private final Viewport viewport;
     private final OrthographicCamera camara;
     public Camara() {
         camara = new OrthographicCamera();
         camara.setToOrtho(false, 50f, 40f); // Ajusta según tu escala y resolución
-        //viewport = new FitViewport(50f, 40f, camara); // O ExtendViewport si lo prefieres
         camara.update();
     }
 
@@ -30,18 +24,4 @@ public class Camara {
         return new Rectangle(camara.position.x - camara.viewportWidth / 2f,
             camara.position.y - camara.viewportHeight / 2f, camara.viewportWidth, camara.viewportHeight);
     }
-
-    /*public void resize(int width, int height) {
-        viewport.update(width, height);
-    }
-
-    /*public Viewport getViewport() {
-        return viewport;
-    }
-
-    public void aplicarYCentrar() {
-        viewport.apply();
-        camara.position.set(viewport.getWorldWidth() / 2f, viewport.getWorldHeight() / 2f, 0);
-        camara.update();
-    }*/
 }
