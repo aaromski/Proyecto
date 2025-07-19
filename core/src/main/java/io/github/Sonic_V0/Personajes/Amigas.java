e33bb6ecc2fc04b64f6eb977f9acd81f06024eed
Miguel <118225922+Miguelord2@users.noreply.github.com>
2025-07-18 20:09:23 -0400

    cambios sobre Knuckles, se toco Etapa, amigas y Mundo

--- core/src/main/java/io/github/Sonic_V0/Personajes/Amigas.java
+++ core/src/main/java/io/github/Sonic_V0/Personajes/Amigas.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 
 public abstract class Amigas extends Personaje {
-    protected  boolean izq = false,  der = false, arr = false, abj = false;  //Estados
+    protected  boolean izq = false,  der = false, arr = false, abj = false;
+    protected boolean golpeando = false;
     protected Animation<TextureRegion> abajo;
     protected Animation<TextureRegion> arriba;
     protected Animation<TextureRegion> diagonalarr;
     protected Animation<TextureRegion> diagonalabj;
+    protected Animation<TextureRegion> golpe;
     protected boolean TLT = false;
     protected boolean ko = false;
 
@@ -18,45 +20,65 @@ public abstract class Amigas extends Personaje {
         super(b);
     }
 
+    public void golpear() {
+        if (!golpeando) {
+            golpeando = true;
+            stateTime = 0;
+        }
+    }
+
     protected void actualizar(float delta) {
-            this.posicion = body.getPosition(); // actualiza posición
-            boolean diagonalArriba = arr && (izq || der);
-            boolean diagonalAbajo = abj && (izq || der);
+        this.posicion = body.getPosition();
 
-            // solo sumar stateTime si se anima
-            if (diagonalArriba || diagonalAbajo || izq || der || abj || arr) {
-                stateTime += delta;
-            } else {
-                stateTime = 0;
+        if (golpeando || arr || abj || izq || der || (arr && (izq || der)) || (abj && (izq || der))) {
+            stateTime += delta;
+        } else {
+            stateTime = 0;
+        }
+
+        boolean diagonalArribaActiva = arr && (izq || der);
+        boolean diagonalAbajoActiva = abj && (izq || der);
+
+        if (golpeando) {
+            frameActual = golpe.getKeyFrame(stateTime);
+            if (golpe.isAnimationFinished(stateTime)) {
+                golpeando = false;
             }
+        } else if (diagonalArribaActiva) {
+            frameActual = diagonalarr.getKeyFrame(stateTime, true);
+        } else if (diagonalAbajoActiva) {
+            frameActual = diagonalabj.getKeyFrame(stateTime, true);
+        } else if (abj) {
+            frameActual = abajo.getKeyFrame(stateTime, true);
+        } else if (arr) {
+            frameActual = arriba.getKeyFrame(stateTime, true);
+        } else if (izq || der) {
+            frameActual = correr.getKeyFrame(stateTime, true);
+        } else {
+            frameActual = sprite;
+        }
 
-            // Lógica de animaciones
-            if (diagonalArriba) {
-                frameActual = diagonalarr.getKeyFrame(stateTime, true);
-                if (der && !frameActual.isFlipX()) frameActual.flip(true, false); // invertir para mirar derecha
-                if (izq && frameActual.isFlipX()) frameActual.flip(true, false); // restaurar si está invertido
-            } else if (diagonalAbajo) {
-                frameActual = diagonalabj.getKeyFrame(stateTime, true);
-                if (izq && !frameActual.isFlipX()) frameActual.flip(true, false); // invertir para mirar izquierda
-                if (der && frameActual.isFlipX()) frameActual.flip(true, false); // restaurar si está invertido
-            } else if (abj) {
-                frameActual = abajo.getKeyFrame(stateTime, true);
-            } else if (arr) {
-                frameActual = arriba.getKeyFrame(stateTime, true);
-            } else if (izq || der) {
-                frameActual = correr.getKeyFrame(stateTime, true);
-            } else {
-                frameActual = sprite;
-            }
-
-            // Flip adicional por si hay inversión en otras direcciones
-            if (!diagonalArriba && !diagonalAbajo) {
-                if (izq && frameActual != null && !frameActual.isFlipX()) {
+        if (frameActual != null) {
+            if (diagonalArribaActiva) {
+                if (izq && !frameActual.isFlipX()) {
                     frameActual.flip(true, false);
-                } else if (der && frameActual != null && frameActual.isFlipX()) {
+                } else if (der && frameActual.isFlipX()) {
+                    frameActual.flip(true, false);
+                }
+            } else if (diagonalAbajoActiva) {
+                if (der && !frameActual.isFlipX()) {
+                    frameActual.flip(true, false);
+                } else if (izq && frameActual.isFlipX()) {
+                    frameActual.flip(true, false);
+                }
+            } else {
+                if (izq && !frameActual.isFlipX()) {
+                    frameActual.flip(true, false);
+                } else if (der && frameActual.isFlipX()) {
                     frameActual.flip(true, false);
                 }
             }
+        }
     }
 
     public void destruir(World world) {
@@ -84,5 +106,4 @@ public abstract class Amigas extends Personaje {
     public void setTLT() {
         TLT = !TLT;
     }
-
 }
