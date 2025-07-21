7ebae6302e4e87bc9537fc05157ad2d0fc4ad582
aarom <aaromarmando3@gmail.com>
2025-07-21 12:24:16 -0400

    21/7/25

--- core/src/main/java/io/github/Sonic_V0/Personajes/Sonic.java
+++ core/src/main/java/io/github/Sonic_V0/Personajes/Sonic.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import io.github.Sonic_V0.Constantes;
--- core/src/main/java/io/github/Sonic_V0/Personajes/Tails.java
+++ core/src/main/java/io/github/Sonic_V0/Personajes/Tails.java
@@ -6,10 +6,8 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
 import io.github.Sonic_V0.Constantes;
 
 public class Tails extends Amigas {
